8803cbbe07724a8f3bc68d96a76dde1ac8ba5b00
Unknown <ckjoooohn@gmail.com>
2017-10-29 11:26:14 -0500

    Update: pipeline and comment

--- ApplicationExample.r
+++ ApplicationExample.r
@@ -7,8 +7,17 @@ library(lattice)
 load("data/.RData") 
 names(df)
 
-#--
+
+#--Examine if nested or fully crossed between factors
+#Tabulate the factors
+xtabs(~ respondent + title, df_HLM)
+
+
+#--Model training
 df_HLM <- select(df, preference, title, respondent, matches("^combined.*ct$"), matches("^tste_3.*ct"))
+
+as.formula
+
 
 model_HLM <- lmer(
   data=df_HLM,
@@ -39,28 +48,31 @@ model_HLMc <- lmer(
 )
 
 model_lm <- lm(data=df_HLM, formula=preference ~ 1 + (combined_autonomy_ct + combined_competence_ct + combined_relatedness_ct) * (1 + tste_3_0_ct + tste_3_1_ct + tste_3_2_ct))
-summary(model_lm)
-BIC(model_lm)
-AIC(model_lm)
 
+
+#--Model performance
 #Chisquare test
 anova(model_HLMa, model_HLMb, model_HLMc, model_HLM, model_lm)
 
 summary(model_lm)
 
 
-#--model fit sensitivity to parameter change
-#Parameter profile deviance confidence interval
-profile_HLMa <- profile(model_HLMa)
+#--Parameter significance
+'
+Depending on the method specified, confint() computes confidence intervals by:
+"profile", "Wald", "boot"
+help("pvalues") formore
+'
+confint(model_HLMa, level=0.95, method="Wald")
 
+#If the diagnal line is streight, the profile interval can be used
 #The vertical lines in the panels delimit the 50%, 80%, 90%, 95% and 99% confidence intervals
+profile_HLMa <- profile(model_HLMa)
 xyplot(profile_HLMa, absVal=FALSE)
-confint(profile_HLMa, level=0.95)
+confint(model_HLMa, level=0.95, method="profile")
 
+
+#--Random effects
 ranef(model_HLMa)
 dotplot(ranef(model_HLMa, condVar=TRUE))
 qqmath(ranef(model_HLMa, condVar=TRUE))
-
-
-#Tabulate the factors
-xtabs(~ respondent + title, df_HLM)
